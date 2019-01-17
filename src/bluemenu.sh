5397cf4de0506493f7ab1bcd07976eba460f4e50
Pack3tL0ss <wade99@gmail.com>
2019-01-16 23:14:23 -0600

    Fix menu error if ctrl char is entered as input

--- src/bluemenu.sh
+++ src/bluemenu.sh
@@ -11,44 +11,8 @@ parity="n"
 dbits=8
 
 do_get_tty_name() {
-        tty_name=$(ls -l /dev |grep lrwx.*ttyUSB0.* |cut -d: -f2|awk '{print $2}')
+        tty_name=$(ls -l /dev |grep lrwx.*${this_tty##*/}.* |cut -d: -f2|awk '{print $2}')
         [[ -z tty_name ]] && tty_name=${this_tty##*/}
-}
-
-menu() {
-    do_flow_pretty
-    do_parity_pretty
-	echo '########################################################################'
-	echo '##                    ConsolePi Connection MENU                       ##'
-	echo ''
-	echo ' This program will launch serial session via picocom'
-	echo ' Be Aware of The following command sequences:'
-	echo ''
-	echo '   ctrl+a followed by ctrl+x Exit session - reset the port'
-	echo '   ctrl+a followed by ctrl+q Exit session - without resetting the port'
-	echo '   ctrl+a followed by ctrl+u increase baud'
-	echo '   ctrl+a followed by ctrl+d decrease baud'
-	echo '   ctrl+a followed by ctrl+f cycle through flow control options'
-	echo '   ctrl+a followed by ctrl+y cycle through parity options'
-	echo '   ctrl+a followed by ctrl+b cycle through data bits'
-	echo '   ctrl+a followed by ctrl+v Show configured port options'
-	echo ''
-	echo " Use command 'consolepi-menu' to display this menu after exit"
-	echo ''
-	echo '##                                                                    ##'
-        echo '########################################################################'
-        echo ''
-		item=1
-		for this_tty in ${tty_list[@]}; do 
-        do_get_tty_name    
-        echo "${item}. Connect to ${tty_name} Using default settings"
-		((item++))
-		done
-
-        echo "c. Change Connection Settings [${baud} ${dbits}${parity_up}1 flow: ${flow_pretty}]"
-        echo 'x. exit'
-        echo ''
-        read -p "Select menu item: " selection
 }
 
 do_flow_pretty() {
@@ -259,39 +223,70 @@ do_change_baud() {
 }
 	
 
-do_verify_selection(){
-valid_selection=false
-while ! $valid_selection; do
-    valid_selection=true
-    if (( $selection > 0 )) && (( $selection < $((${#tty_list[@]}+1)) )); then
-		this_tty="${tty_list[$((selection - 1))]}"
-                do_get_tty_name
-		# screen "/dev/${tty_list[$((selection - 1))]##*/}" $baud
-		picocom "/dev/${tty_name}" -b $baud
-                consolepi-menu && exit 0
+main_menu() {
+    clear
+    valid_selection=false
+    while ! $valid_selection; do
 
+    do_flow_pretty
+    do_parity_pretty
+	echo '########################################################################'
+	echo '##                    ConsolePi Connection MENU                       ##'
+	echo ''
+	echo ' This program will launch serial session via picocom'
+	echo ' Be Aware of The following command sequences:'
+	echo ''
+	echo '   ctrl+a followed by ctrl+x Exit session - reset the port'
+	echo '   ctrl+a followed by ctrl+q Exit session - without resetting the port'
+	echo '   ctrl+a followed by ctrl+u increase baud'
+	echo '   ctrl+a followed by ctrl+d decrease baud'
+	echo '   ctrl+a followed by ctrl+f cycle through flow control options'
+	echo '   ctrl+a followed by ctrl+y cycle through parity options'
+	echo '   ctrl+a followed by ctrl+b cycle through data bits'
+	echo '   ctrl+a followed by ctrl+v Show configured port options'
+	echo '   ctrl+a followed by ctrl+c toggle local echo'
+	echo ''
+	echo " Use command 'consolepi-menu' to display this menu after exit"
+	echo ''
+	echo '##                                                                    ##'
+        echo '########################################################################'
+        echo ''
+		item=1
+		for this_tty in ${tty_list[@]}; do 
+        do_get_tty_name    
+        echo "${item}. Connect to ${tty_name} Using default settings"
+		((item++))
+		done
+
+        echo "c. Change Connection Settings [${baud} ${dbits}${parity_up}1 flow: ${flow_pretty}]"
+        echo 'x. exit to shell'
+        echo ''
+        read -p "Select menu item: " selection
+
+    #if selection not defined or selection is non-printable cntrl char set to zero to fail through without error
+    ( [[ -z $selection ]] || [[ $selection =~ [[:cntrl:]] ]] ) && selection=0
+    if (( $selection > 0 )) && (( $selection < $((${#tty_list[@]}+1)) )); then
+	this_tty="${tty_list[$((selection - 1))]}"
+        do_get_tty_name
+	# screen "/dev/${tty_list[$((selection - 1))]##*/}" $baud
+	picocom "/dev/${tty_name}" -b $baud
     elif [[ ${selection,,} == "c" ]]; then
         valid_selection=false # force re-print of menu
-		port_config_menu
-		menu
-		
+	port_config_menu
     elif [[ ${selection,,} == "x" ]]; then
+        valid_selection=true
         exit 0
-		
     else
         valid_selection=false
         echo -e "\nInvalid Selection Try Again\n\n"
-        menu
     fi
 done
 }
 
 main() {
 	[[ -z $tty_list ]] && echo "No USB to Serial adapters found... exiting" && exit 1
-		[[ -z $(picocom --help | head -1) ]] && echo "this program requires picocom, install picocom 'sudo apt-get install picocom' ... exiting" && exit 1
-		clear
-        menu
-        do_verify_selection
+	[[ -z $(picocom --help | head -1) ]] && echo "this program requires picocom, install picocom 'sudo apt-get install picocom' ... exiting" && exit 1
+        main_menu
 }
 
 main

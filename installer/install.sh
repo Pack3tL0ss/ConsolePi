542e0d73fea25007e99315629bbff574cb91dd47
Pack3tL0ss <wade99@gmail.com>
2019-01-16 23:22:43 -0600

    update installer to use picocom for local connections

--- installer/install.sh
+++ installer/install.sh
@@ -880,26 +880,33 @@ EOF
 	#create rfcomm device if not already there
 	rfdev=$( rfdev=(/dev/rfcomm*) && echo ${rfdev##*/} )
 	[[ $rfdev =~ '*' ]] && rfdev=
-	if [[ -z $rfdev ]]; then
-	    bt_mac=$(sudo hciconfig |grep "BD Address" |awk '{print $3}')
-		sudo rfcomm bind 0 ${bt_mac} && logit "${process}" "bluetooth device now assigned to ${rfdev}" || logit "${process}" "Failed to assign bluetooth device to ${rfdev}"
-	else
-		logit "${process}" "bluetooth device ${rfdev} exists"
-	fi
+	# This should be the bluetooth MAC of the PC ConsolePi is paired with
+	# if [[ -z $rfdev ]]; then
+	    # bt_mac=$(sudo hciconfig |grep "BD Address" |awk '{print $3}')
+		# sudo rfcomm bind 0 ${bt_mac} && logit "${process}" "bluetooth device now assigned to ${rfdev}" || logit "${process}" "Failed to assign bluetooth device to ${rfdev}"
+	# else
+		# logit "${process}" "bluetooth device ${rfdev} exists"
+	# fi
     
     # add blue user and set to launch menu on login
     echo -e 'ConsoleP1!!\nConsoleP1!!\n' | sudo adduser --gecos "" blue 1>/dev/null 2>> $tmp_log 
-    sudo echo consolepi-menu | sudo tee -a /home/blue/.bashrc > /dev/null
-    [[ $(grep consolepi-menu /home/blue/.bashrc) ]] && logit "${process}" "BlueTooth User Configured to launch menu on login" || 
-                logit "${process}" "FAILED to enable menu on login for BlueTooth User" "WARNING"
-
-    # Install Screen
-    if [[ $(screen -v 2>/dev/null | awk '{print $3}') ]]; then 
-        logit "${process}" "Screen $(screen -v | awk '{print $3}') is already installed"
+	sudo usermod -a -G dialout blue 2>> $tmp_log && logit "${process}" "BlueTooth User added to dialout group" || 
+		logit "${process}" "FAILED to add Bluetooth user to dialout group" "WARNING"
+    if [[ ! $(sudo grep consolepi-menu /home/blue/.bashrc) ]]; then
+        sudo echo consolepi-menu | sudo tee -a /home/blue/.bashrc > /dev/null && 
+            logit "${process}" "BlueTooth User Configured to launch menu on Login" || 
+            logit "${process}" "FAILED to enable menu on login for BlueTooth User" "WARNING"
     else
-        logit "${process}" "Installing Screen"
-        sudo apt-get -y install screen 1>/dev/null 2>> $tmp_log && logit "${process}" "Install Screen Success" || 
-                logit "${process}" "FAILED to Install Screen" "WARNING"
+        logit "${process}" "blue user already configured to launch menu on Login"
+	fi
+
+    # Install picocom
+    if [[ $(picocom --help 2>/dev/null | head -1) ]]; then 
+        logit "${process}" "picocom $(picocom --help 2>/dev/null | head -1) is already installed"
+    else
+        logit "${process}" "Installing picocom"
+        sudo apt-get -y install picocom 1>/dev/null 2>> $tmp_log && logit "${process}" "Install picocom Success" || 
+                logit "${process}" "FAILED to Install picocom" "WARNING"
     fi
        
     logit "${process}" "${process} Complete"

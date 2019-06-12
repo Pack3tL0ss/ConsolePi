18db9466199f9bfd830b85ff855424faf1a9a2f2
Pack3tL0ss <wade99@gmail.com>
2019-06-12 18:07:51 -0500

    test dhcp trigger

---
+++ src/dhcp-trigger.py
@@ -0,0 +1,16 @@
+#!/etc/ConsolePi/venv/bin/python3
+
+from consolepi.common import check_reachable
+import sys
+import os
+from consolepi.common import ConsolePi_Log
+
+log = ConsolePi_Log().set_log()
+
+ip = sys.argv[3]
+vendor = os.environ['DNSMASQ_VENDOR_CLASS']
+
+ssh = check_reachable(ip, 22)
+telnet = check_reachable(ip, 23)
+
+log('{} {} {} {}'.format(ip, vendor, ssh, telnet))

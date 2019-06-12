f00aa3a8bf1325be235c727b8ff05031b8cc83fa
Pack3tL0ss <wade99@gmail.com>
2019-06-12 18:51:02 -0500

    update dhcp-trigger

--- src/dhcp-trigger.py
+++ src/dhcp-trigger.py
@@ -1,9 +1,12 @@
 #!/etc/ConsolePi/venv/bin/python3
 
 from consolepi.common import check_reachable
+# from consolepi.common import get_config
 import sys
 import os
 from consolepi.common import ConsolePi_Log
+
+match = ['2530', '3810']
 
 log = ConsolePi_Log().set_log()
 
@@ -16,4 +19,11 @@ except KeyError:
 ssh = check_reachable(ip, 22)
 telnet = check_reachable(ip, 23)
 
-log('{} {} {} {}'.format(ip, vendor, ssh, telnet))
+log.info('{} {} {} {}'.format(ip, vendor, ssh, telnet))
+
+for _ in match:
+    if _ in vendor:
+        if ssh:
+            log.info('{} is reachable via ssh @ {}'.format(_, ip))
+        elif telnet:
+            log.info('{} is reachable via telnet @ {}'.format(_, ip))

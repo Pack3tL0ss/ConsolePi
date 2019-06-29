f35d24a2efddd3e02ffe8c0523bd961c978bf3cc
Wade <wade99@gmail.com>
2019-06-29 15:51:33 -0500

    Update venv with modules for API and pyudev

--- src/dhcp-trigger.py
+++ src/dhcp-trigger.py
@@ -4,27 +4,47 @@ from consolepi.common import check_reachable
 # from consolepi.common import get_config
 import sys
 import os
+import json
 from consolepi.common import ConsolePi_Log
+from consolepi.common import get_local
+
+with open('/home/pi/dhcp-test.out', 'a') as new_file:
+    new_file.write(str(sys.argv[1:]) + '\n' + str(os.environ) + '\n')
 
 match = ['2530', '3810']
 
 log = ConsolePi_Log().set_log()
 
+
+add_del = sys.argv[1]
 mac = sys.argv[2]
 ip = sys.argv[3]
+
+
+try:
+    iface = os.environ['DNSMASQ_INTERFACE']
+except KeyError:
+    iface = None
 try:
     vendor = os.environ['DNSMASQ_VENDOR_CLASS']
 except KeyError:
     vendor = None
 
-ssh = check_reachable(ip, 22)
-telnet = check_reachable(ip, 23)
+log.info('{},  iface: {}, mac: {}, ip: {}, vendor: {}'.format(add_del, iface, mac, ip, vendor))
 
-log.info('{} {} {} {}'.format(ip, vendor, ssh, telnet))
+# oobm Discovery & ZTP trigger
+if iface is not None and iface == 'eth0':
+    ssh = check_reachable(ip, 22)
+    telnet = check_reachable(ip, 23)
 
-for _ in match:
-    if _ in vendor:
-        if ssh:
-            log.info('{} is reachable via ssh @ {}'.format(_, ip))
-        elif telnet:
-            log.info('{} is reachable via telnet @ {}'.format(_, ip))
+    log.info('{} {} {} {}'.format(ip, vendor, ssh, telnet))
+
+    for _ in match:
+        if _ in vendor:
+            if ssh:
+                log.info('{} is reachable via ssh @ {}'.format(_, ip))
+            elif telnet:
+                log.info('{} is reachable via telnet @ {}'.format(_, ip))
+
+if vendor is not None and 'ConsolePi' in vendor:
+    log.info('ConsolePi Discovered via DHCP')

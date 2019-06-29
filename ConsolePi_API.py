02d5e2e4f68b98a771562cf55512e32745206c62
Wade <wade99@gmail.com>
2019-06-29 15:50:03 -0500

    Add Rest API

---
+++ ConsolePi_API.py
@@ -0,0 +1,33 @@
+#!venv/bin/python3
+
+import socket
+from flask import Flask, jsonify
+from consolepi.common import ConsolePi_Log
+from consolepi.common import get_local
+from consolepi.common import get_if_ips
+from consolepi.common import get_local_cloud_file
+
+app = Flask(__name__)
+CPI_LOG = ConsolePi_Log(do_print=False)
+log = CPI_LOG.log
+# adapters = get_local(log)
+LOCAL_CLOUD_FILE = '/etc/ConsolePi/cloud.data'
+
+@app.route('/api/v1.0/adapters', methods=['GET'])
+def get_adapters():
+    return jsonify({'adapters': get_local(cpi_log=CPI_LOG, do_print=False)})
+
+@app.route('/api/v1.0/remcache', methods=['GET'])
+def get_cache():
+    return jsonify({'remotes': get_local_cloud_file(LOCAL_CLOUD_FILE)})
+
+@app.route('/api/v1.0/ifaces', methods=['GET'])
+def get_ifaces():
+    return jsonify({'interfaces': get_if_ips(log)})
+
+@app.route('/api/v1.0/details', methods=['GET'])
+def get_details():
+    return jsonify({socket.gethostname(): {'adapters': get_local(cpi_log=CPI_LOG, do_print=False), 'interfaces': get_if_ips(log)}})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)

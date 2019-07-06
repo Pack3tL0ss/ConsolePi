30d2e4f98489e4e2e5296c8350487270420f4c54
Wade <wade99@gmail.com>
2019-07-06 17:28:34 -0500

    refactor to use new ConsolePi_data class

--- cloud/gdrive/cloud.py
+++ cloud/gdrive/cloud.py
@@ -1,37 +1,22 @@
 #!/etc/ConsolePi/venv/bin/python3
 
-from socket import gethostname
-from consolepi.common import get_config
-from consolepi.common import get_if_ips
-from consolepi.common import get_local
-from consolepi.common import update_local_cloud_file
-from consolepi.common import ConsolePi_Log
+from consolepi.common import ConsolePi_data
 from consolepi.gdrive import GoogleDrive
 
-DEBUG = get_config('debug')
-CLOUD_SVC = get_config('cloud_svc').lower()
-LOG_FILE = '/var/log/ConsolePi/cloud.log'
-LOCAL_CLOUD_FILE = '/etc/ConsolePi/cloud.data'
-
+config = ConsolePi_data(do_print=False)
 
 def main():
-    cpi_log = ConsolePi_Log(debug=DEBUG, log_file=LOG_FILE)
-    log = cpi_log.log
-    hostname = gethostname()
-    if_ips = get_if_ips(log)
-    tty_list = get_local(cpi_log=cpi_log)
-    data = {hostname: {'user': 'pi'}}
-    data[hostname]['adapters'] = tty_list
-    data[hostname]['interfaces'] = if_ips
-    log.debug('Final Data set collected for {}: {}'.format(hostname, data))
+    log = config.log
+    data = config.local
+    log.debug('Final Data set collected for {}: {}'.format(config.hostname, data))
     log.info('Cloud Update triggered by IP update')
-    if CLOUD_SVC == 'gdrive':
+    if config.cloud_svc == 'gdrive':
         cloud = GoogleDrive(log)
     remote_consoles = cloud.update_files(data)
 
-    # NEW Python Menu: Write All Remotes to local file
+    # Send remotes learned from cloud file to local cache
     if len(remote_consoles) > 0:
-        update_local_cloud_file(LOCAL_CLOUD_FILE, remote_consoles)
+        config.update_local_cloud_file(remote_consoles)
 
 
 if __name__ == '__main__':

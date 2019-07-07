1cb0150406eaef742230b6cac68550132c4a861a
Wade <wade99@gmail.com>
2019-07-07 04:15:19 -0500

    refactor to use new ConsolePi_data class

--- ConsolePi_API.py
+++ ConsolePi_API.py
@@ -1,34 +1,28 @@
 #!venv/bin/python3
 
-import socket
 from waitress import serve
 from flask import Flask, jsonify
-from consolepi.common import ConsolePi_Log
-from consolepi.common import get_local
-from consolepi.common import get_if_ips
-from consolepi.common import get_local_cloud_file
+from consolepi.common import ConsolePi_data
 
 app = Flask(__name__)
-CPI_LOG = ConsolePi_Log(do_print=False)
-log = CPI_LOG.log
-# adapters = get_local(log)
-LOCAL_CLOUD_FILE = '/etc/ConsolePi/cloud.data'
+config = ConsolePi_data(do_print=False)
+log = config.log
 
 @app.route('/api/v1.0/adapters', methods=['GET'])
 def get_adapters():
-    return jsonify({'adapters': get_local(cpi_log=CPI_LOG, do_print=False)})
+    return jsonify({'adapters': config.adapters})
 
 @app.route('/api/v1.0/remcache', methods=['GET'])
 def get_cache():
-    return jsonify({'remotes': get_local_cloud_file(LOCAL_CLOUD_FILE)})
+    return jsonify({'remotes': config.remotes})
 
 @app.route('/api/v1.0/ifaces', methods=['GET'])
 def get_ifaces():
-    return jsonify({'interfaces': get_if_ips(log)})
+    return jsonify({'interfaces': config.interfaces})
 
 @app.route('/api/v1.0/details', methods=['GET'])
 def get_details():
-    return jsonify({socket.gethostname(): {'adapters': get_local(cpi_log=CPI_LOG, do_print=False), 'interfaces': get_if_ips(log), 'user': 'pi'}})
+    return jsonify(config.local)
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=5000)

26a737423236ac76bf59eda3c9a71df821620b7e
Wade <wade99@gmail.com>
2019-08-15 17:26:59 -0500

    Tinkering with a remote kill function for hung rem sessions

--- installer/common.sh
+++ installer/common.sh
@@ -255,8 +255,8 @@ get_ser2net() {
 do_kill_hung_ssh() {
     dev_name=${1##*/}
     echo $HOSTNAME $dev_name - ${1} - $0
-    proc=$(ps auxf | grep -v grep | grep -v "$0" | grep "$dev_name" | awk '{print $2}')
-    sudo pkill -SIGTERM -ns ${proc##*-}
+    proc=$(ps auxf | grep -v grep | grep -v "${0##*-}" | grep "$dev_name" | awk '{print $2}')
+    sudo pkill -SIGTERM -ns $proc
     ps auxf
     echo $proc
 }

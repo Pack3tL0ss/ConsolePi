dfb9a27e6bf58cf4d5688117bef043aa02dcce93
Wade <wade99@gmail.com>
2019-08-16 20:44:56 -0500

    Add Launcher script to detect hung sessions

--- src/remote_launcher.py
+++ src/remote_launcher.py
@@ -2,6 +2,8 @@
 
 import psutil
 import sys
+import subprocess
+from time import sleep
 
 def find_procs_by_name(name, dev):
     "Return a list of processes matching 'name'."
@@ -22,8 +24,15 @@ def terminate_process(pid):
             break
         x += 1
 
-if len(sys.argv) == 3:
+if len(sys.argv) >= 3:
+    # _cmd = (' '.join(sys.argv[1:]))
+    _cmd = sys.argv[1:]
     ppid = find_procs_by_name(sys.argv[1], sys.argv[2])
-    if ppid is not None:
+    retry = 0
+    while ppid is not None and retry < 3:
+        print('An Existing session is already established to {}.  Terminating that session'.format(sys.argv[2].replace('/dev/', '')))
         terminate_process(ppid)
+        sleep(1)
+        ppid = find_procs_by_name(sys.argv[1], sys.argv[2])
     
+    subprocess.run(_cmd)

381065b138b88f9d25a64f71adf5455c02f2b6b4
Wade <wade99@gmail.com>
2019-08-18 03:51:09 -0500

    minor improvements

--- src/consolepi-wlanreset.sh
+++ src/consolepi-wlanreset.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+wifidev=wlan0
+ethdev=eth0
 
 wpa_cli terminate
 ip addr flush wlan0
 ip link set dev wlan0 down
 rm -r /var/run/wpa_supplicant 2>/dev/null
 rm -r /tmp/wlan0 2>/dev/null
-wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
+ip link set dev wlan0 up
+wpa_supplicant -B -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf

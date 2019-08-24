37fd9561719f7cfcf749dff1781c9524b6603a89
Wade <wade99@gmail.com>
2019-08-24 03:23:35 -0500

    Function to generate file based on template then compare

---
+++ j2render.py
@@ -0,0 +1,34 @@
+#!/etc/ConsolePi/venv/bin/python3
+'''
+Convert ConsolePi system file template to final state
+
+params:
+argv[1] template name without j2 extension (file should be in /etc/ConsolePi/src/j2)
+argv[2] ignored by this script used by installer which compares resulting template to file @ argv[2] and updates if different
+remaining arguments (3+) should be in form template_var=desired_value
+
+Output:
+Creates file in /tmp/ directory (/tmp/argv[1]) used to compare against system file specified in argv[2] by install script.
+'''
+import jinja2
+import sys
+
+def parse_args():
+    parameter_dict = {}
+    for user_input in sys.argv[3:]:
+        if "=" not in user_input:
+            continue
+        varname = user_input.split("=")[0]
+        varvalue = user_input.split("=")[1]
+        parameter_dict[varname] = varvalue
+    return parameter_dict
+
+parameter_dict = parse_args()
+templateLoader = jinja2.FileSystemLoader(searchpath="/etc/ConsolePi/src/j2/")
+templateEnv = jinja2.Environment(loader=templateLoader)
+template = templateEnv.get_template(sys.argv[1] + '.j2')
+# outputText = template.render(wlan_ssid='ConsolePi', wlan_psk='hpIMC!!!', wlan_country='US')
+outputText = template.render(parameter_dict)
+template.stream(parameter_dict).dump('/tmp/{}'.format(sys.argv[1]))
+
+# print(outputText)

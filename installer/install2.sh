ae7d3e385c1d887a139ddb437f0875875de92834
Wade Wells <wade99@gmail.com>
2019-12-06 22:29:48 -0600

    backup any custom consolepi- commands found in /usr/local/bin

--- installer/install2.sh
+++ installer/install2.sh
@@ -983,6 +983,7 @@ get_known_ssids() {
 do_consolepi_commands() {
     process="Remove old consolepi-commands from /usr/local/bin"
     if [ $(ls -l /usr/local/bin/consolepi* 2>/dev/null | wc -l) -ne 0 ]; then
+        sudo cp /usr/local/bin/consolepi-* $bak_dir 2>>$log_file || logit "Failed to Backup potentially custom consolepi-commands in /usr/local/bin"
         sudo rm /usr/local/bin/consolepi-* > /dev/null 2>&1
         sudo unlink /usr/local/bin/consolepi-* > /dev/null 2>&1
         [ $(ls -l /usr/local/bin/consolepi* 2>/dev/null | wc -l) -eq 0 ] &&

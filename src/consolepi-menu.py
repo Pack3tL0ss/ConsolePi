786e2375b60e99da09cb74aad9b0cd265b386a43
Wade Wells <wade99@gmail.com>
2019-12-07 02:22:04 -0600

    FIX resize for remote sessions

--- src/consolepi-menu.py
+++ src/consolepi-menu.py
@@ -1568,7 +1568,8 @@ class ConsolePiMenu():
                                 if _error:
                                     self.error_msgs.append(_error)
                             # -- // resize the terminal to handle serial connections that jack the terminal size \\ --
-                            if 'picocom' in c or config.REM_LAUNCH in c:
+                            c = ' '.join([str(i) for i in c])
+                            if 'picocom' in c: # pylint: disable=maybe-no-member
                                 os.system('/etc/ConsolePi/src/consolepi-commands/resize >/dev/null')
                         except KeyboardInterrupt:
                             self.error_msgs.append('Aborted last command based on user input')

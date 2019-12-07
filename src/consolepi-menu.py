b4ce4d0f514dd64d7c8597d56f490db2524be636
Wade Wells <wade99@gmail.com>
2019-12-06 22:55:40 -0600

    fix syntax

--- src/consolepi-menu.py
+++ src/consolepi-menu.py
@@ -1303,14 +1303,14 @@ class ConsolePiMenu():
     def show_serial_prompt(self, adapter):
         with Halo(text='Prompt Displayed on Port: ', spinner='dots1', placement='right'):
             p = None
-            for i in range(2)
-            try:
-                p = get_serial_prompt(adapter)
-                e = self.format_line('{{red}}No text was rcvd from port{{norm}}')[1]
-            except SerialException as e:
-                e = self.format_line('{{red}}Exception Occured Trying To access port{{norm}}')[1]
-                e+='\n{}'.format(e)
-                self.trigger_udev()
+            for i in range(2):
+                try:
+                    p = get_serial_prompt(adapter)
+                    e = self.format_line('{{red}}No text was rcvd from port{{norm}}')[1]
+                except SerialException as e:
+                    e = self.format_line('{{red}}Exception Occured Trying To access port{{norm}}')[1]
+                    e+='\n{}'.format(e)
+                    self.trigger_udev()
 
         print('Prompt Displayed on Port: {}'.format(p if p else e))
         

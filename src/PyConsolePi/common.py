c15565748ffd4a510984c2458a52ccea276899a0
Wade Wells <wade99@gmail.com>
2019-12-07 02:01:26 -0600

    ensure scripts run with consolepi-commands in path

--- src/PyConsolePi/common.py
+++ src/PyConsolePi/common.py
@@ -39,6 +39,8 @@ HOME = str(Path.home())
 POWER_FILE = '/etc/ConsolePi/power.json'
 VALID_BAUD = ['300', '1200', '2400', '4800', '9600', '19200', '38400', '57600', '115200']
 REM_LAUNCH = '/etc/ConsolePi/src/remote_launcher.py'
+if not 'consolepi-commands' in os.environ["PATH"]:
+    os.environ["PATH"] += os.pathsep + '/etc/ConsolePi/src/consolepi-commands'
 
 class ConsolePi_Log:
 

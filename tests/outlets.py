739ac07023d8e02e98bc5ba9522c333945419351
wade ~ Pack3tL0ss <wade99 at github>
2020-02-11 12:09:19 -0600

    initial commit

---
+++ tests/outlets.py
@@ -0,0 +1,40 @@
+#!/etc/ConsolePi/venv/bin/python3
+
+import json
+import sys
+# import threading
+sys.path.insert(0, '/etc/ConsolePi/src/pypkg')
+
+from consolepi import ConsolePi  # NoQA
+from consolepi import threading  # NoQA
+# from consolepi.power.outlets import Outlets  # NoQA
+
+cpi = ConsolePi()
+pwr = cpi.pwr
+utils = cpi.utils
+threading.Thread(target=utils.set_perm, args=['/home/pi/testfile'], name='common_import_test').start()
+# outlet_data = cpi.config.outlets
+pwr.pwr_start_update_threads()
+if cpi.wait_for_threads():
+    print('Threads Timed Out')
+    sys.exit(1)
+else:
+    outlets = pwr.data
+
+if len(sys.argv) >= 2:
+    if len(sys.argv) == 2:
+        print(json.dumps(getattr(pwr, sys.argv[1]), indent=4, sort_keys=True))
+    else:
+        func = getattr(pwr, sys.argv[1])
+        print(sys.argv[2:])
+        func(*sys.argv[2:])
+        # upd = pwr.pwr_get_outlets(upd_linked=True)
+        # print(json.dumps(upd, indent=4, sort_keys=True))
+else:
+    print(json.dumps(outlets, indent=4, sort_keys=True))
+    print(outlets['defined']['labpower1']['linked_devs'])
+
+# for t in threading.enumerate():
+#     print(t.name)
+for e in cpi.error_msgs:
+    print(e)

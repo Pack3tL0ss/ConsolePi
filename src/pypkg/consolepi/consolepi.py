b9f5243c680d211935092c77c5e4d97bf43e8669
wade ~ Pack3tL0ss <wade99 at github>
2020-03-04 23:16:08 -0600

    refactor exec_auto_pwron ++ other

--- src/pypkg/consolepi/consolepi.py
+++ src/pypkg/consolepi/consolepi.py
@@ -3,6 +3,8 @@
 import threading
 import time
 import os
+import json
+import subprocess
 from consolepi.power import Outlets
 from consolepi import Config
 from consolepi.remotes import Remotes
@@ -27,6 +29,9 @@ class ConsolePi():
         self.log = self.config.log
         self.local = Local(self)
         self.remotes = Remotes(self)
+        # used to signal the need to retry connection if autopwr for ssh/telnet sessions (allow bootup)
+        # changed in auto_pwron_thread
+        self.autopwr_wait = False
         if self.config.cfg.get('power'):
             self.pwr_init_complete = False
             self.pwr = Outlets(self)
@@ -43,22 +48,27 @@ class ConsolePi():
         res = namedtuple('Response', 'baud dbits parity flow sbits error')
         return res(ok, baud, dbits, parity, flow, sbits, error)
 
-    def exec_auto_pwron(self, menu_dev):
+    def exec_auto_pwron(self, pwr_key):
         '''Launch auto_pwron in thread
 
         params:
             menu_dev:str, The tty dev user is connecting to
         '''
-        print('Checking for and Powering on any outlets linked to {} in the background'.format(menu_dev.replace('/dev/', '')))
-        # self.auto_pwron_thread(menu_dev) # swap to debug directly (disable thread)
-        threading.Thread(target=self.auto_pwron_thread, args=(menu_dev,),
-                         name='auto_pwr_on_' + menu_dev.replace('/dev/', '')).start()
-        self.log.debug('[AUTO PWRON] Active Threads: {}'.format(
-            [t.name for t in threading.enumerate() if t.name != 'MainThread']
-            ))
+        pwr_key_pretty = pwr_key.replace('/dev/', '').replace('/host/', '')
+        if pwr_key in self.config.outlets['linked']:  # verify against config here as pwr may not be completely init
+            _msg = f"Ensuring {pwr_key_pretty} Linked Outlets ({' '.join(self.config.outlets['linked'][pwr_key])}) " \
+                   "are Powered \033[1;32mON\033[0m"
+            _dots = '-' * (len(_msg) + 4)
+            _msg = f"\n{_dots}\n  {_msg}  \n{_dots}\n"  # TODO send to formatter in menu ... __init__
+            print(_msg)
+            threading.Thread(target=self.auto_pwron_thread, args=(pwr_key,),
+                             name='auto_pwr_on_' + pwr_key_pretty).start()
+            self.log.debug('[AUTO PWRON] Active Threads: {}'.format(
+                [t.name for t in threading.enumerate() if t.name != 'MainThread']
+                ))
 
     # TODO just get the outlet from the dict, and pass to power module function let it determine type etc
-    def auto_pwron_thread(self, menu_dev):
+    def auto_pwron_thread(self, pwr_key):
         '''Ensure any outlets linked to device are powered on
 
         Called by consolepi_menu exec_menu function and remote_launcher (for sessions to remotes)
@@ -70,55 +80,85 @@ class ConsolePi():
         Returns:
             No Return - Updates class attributes
         '''
-        config = self.config
+        # config = self.config
         log = self.log
         if not self.pwr_init_complete:
-            if not self.wait_for_threads('init'):
-                self.config.log_and_show('Timeout Waiting for Power threads', log=log.error)
-        for adapter in config.outlets.get('linked', []):
-            outlet = self.pwr.data.get(adapter)  # TODO need split logic for ports with new schema
-            if not outlet:
-                continue
+            if self.wait_for_threads('init'):
+                return
 
+        outlets = self.pwr.data
+        if 'linked' not in outlets:
+            _msg = 'Error linked key not found in outlet dict\nUnable to perform auto power on'
+            self.config.log_and_show(_msg, log=log.error)
+            return
+
+        if not outlets['linked'].get(pwr_key):
+            return
+
+        # -- // Perform Auto Power On (if not already on) \\ --
+        for o in outlets['linked'][pwr_key]:
+            outlet = outlets['defined'].get(o.split(':')[0])
+            ports = [] if ':' not in o else json.loads(o.split(':')[1])
             _addr = outlet['address']
 
             # -- // DLI web power switch Auto Power On \\ --
             if outlet['type'].lower() == 'dli':
-                for p in outlet['is_on']:
-                    log.debug('[Auto PwrOn] Power ON {} Linked Outlet {}:{} p{}'.format(menu_dev, outlet['type'], _addr, p))
+                for p in ports:
+                    log.debug(f"[Auto PwrOn] Power ON {pwr_key} Linked Outlet {outlet['type']}:{_addr} p{p}")
                     if not outlet['is_on'][p]['state']:   # This is just checking what's in the dict not querying the DLI
-                        r = self.pwr.pwr_toggle(outlet['type'], outlet['address'], desired_state=True, port=p)
+                        r = self.pwr.pwr_toggle(outlet['type'], _addr, desired_state=True, port=p)
                         if isinstance(r, bool):
                             if r:
                                 threading.Thread(target=self.outlet_update, kwargs={'refresh': True,
                                                  'upd_linked': True}, name='auto_pwr_refresh_dli').start()
+                                self.autopwr_wait = True
                         else:
-                            log.warning('{} Error operating linked outlet @ {}'.format(menu_dev, outlet['address']))
-                            self.error_msgs.append('Error operating linked outlet @ {}'.format(outlet['address']))
+                            self.config.log_and_show(f"{pwr_key} Error operating linked outlet @ {o}", log=log.warning)
 
             # -- // GPIO & TASMOTA Auto Power On \\ --
             else:
-                log.debug('[Auto PwrOn] Power ON {} Linked Outlet {}:{}'.format(menu_dev, outlet['type'], _addr))
-                r = self.pwr.pwr_toggle(outlet['type'], outlet['address'], desired_state=True,
-                                        noff=outlet['noff'] if outlet['type'].upper() == 'GPIO' else True)
-                # TODO Below (first if) should never happen fail-safe after refactoring the returns from power.py
-                # Can eventually remove
-                if not isinstance(r, bool) and isinstance(r, int) and r <= 1:
-                    self.error_msgs.append('the return from {} {} was an int({})'.format(
-                        outlet['type'], outlet['address'], r
-                    ))
-                elif isinstance(r, int) and r > 1:  # return is an error
+                log.debug(f"[Auto PwrOn] Power ON {pwr_key} Linked Outlet {outlet['type']}:{_addr}")
+                r = self.pwr.pwr_toggle(outlet['type'], _addr, desired_state=True,
+                                        noff=outlet.get('noff', True) if outlet['type'].upper() == 'GPIO' else True)
+                if isinstance(r, int) and r > 1:  # return is an error
                     r = False
                 else:   # return is bool which is what we expect
                     if r:
-                        self.pwr.outlet_data['linked'][outlet['key']]['state'] = r
-                        self.outlets = self.pwr.outlet_data['linked']
-                        self.pwr.pwr_get_outlets(upd_linked=True)
+                        self.pwr.data['defined'][o]['state'] = r
+                        self.autopwr_wait = True
+                        # self.pwr.pwr_get_outlets(upd_linked=True)
                     else:
-                        self.error_msgs.append('Error operating linked outlet @ {}'.format(outlet['address']))
-                        log.warning('{} Error operating linked outlet @ {}'.format(menu_dev, outlet['address']))
+                        self.config.log_and_show(f"Error operating linked outlet {o}:{outlet['address']}", log=log.warning)
 
-    def wait_for_threads(self, name='init', timeout=8, thread_type='power'):
+    def exec_shell_cmd(self, cmd):
+        '''Determine if cmd is valid shell cmd and execute if so.
+
+        Command will execute as the local user unless the user used sudo -u
+
+        Arguments:
+            cmd {str} -- Input provided by user
+
+        Returns:
+            True|None -- Return True if cmd was determined to be a bash cmd
+        '''
+        s = subprocess
+        c = cmd.replace('-u', '').replace('sudo', '').strip()
+        p = 'PATH=$PATH:/etc/ConsolePi/src/consolepi-commands && '
+        r = s.run(f'{p}which {c.split()[0]}', shell=True, capture_output=True)
+        if r.returncode == 0:
+            try:
+                if 'sudo ' not in cmd:
+                    cmd = f'sudo -u {self.local.user} bash -c "{p}{cmd}"'
+                elif 'sudo -u ' not in cmd:
+                    cmd = cmd.replace('sudo ', '')
+                subprocess.run(cmd, shell=True)
+            except (KeyboardInterrupt, EOFError):
+                pass
+            print('')
+            input('Press Enter to Continue... ')
+            return True
+
+    def wait_for_threads(self, name='init', timeout=10, thread_type='power'):
         '''wait for parallel async threads to complete
 
         returns:
@@ -146,13 +186,13 @@ class ConsolePi():
                         name.strip('_').upper(), thread_type.upper(), time.time() - start))
                 break
             elif time.time() - start > timeout:
-                log.error('[{0} {1} WAIT] Timeout Waiting for {0} Threads to Complete, elapsed time: {2}'.format(
-                    name.strip('_').upper(), thread_type.upper(), time.time() - start))
+                self.config.log_and_show('[{0} {1} WAIT] Timeout Waiting for {0} Threads to Complete, elapsed time: {2}'.format(
+                    name.strip('_').upper(), thread_type.upper(), time.time() - start), log=log.error)
                 return True
 
     def launch_shell(self):
         iam = self.config.loc_user
-        os.system('sudo -u {0} echo PS1=\\"consolepi-menu:\\\w\\\$ \\" >/tmp/prompt && '  # NoQA
+        os.system('sudo -u {0} echo PS1=\\"\\\033[1\;36mconsolepi-menu\\\033[0m:\\\w\\\$ \\" >/tmp/prompt && '  # NoQA
             'echo alias consolepi-menu=\\"exit\\" >>/tmp/prompt &&'
             'echo PATH=$PATH:/etc/ConsolePi/src/consolepi-commands >>/tmp/prompt && '
             'alias consolepi-menu=\\"exit\\" >>/tmp/prompt && '

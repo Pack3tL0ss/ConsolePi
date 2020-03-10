2e114cf42972d4a0c0b253bcc9de81fb7b47213d
wade ~ Pack3tL0ss <wade99 at github>
2020-03-09 23:48:10 -0500

    Continue work on breaking up into modules

--- src/pypkg/consolepi/remotes.py
+++ src/pypkg/consolepi/remotes.py
@@ -3,55 +3,62 @@
 import json
 import requests
 import threading
+import socket
 from halo import Halo
 import time
 from sys import stdin
 from log_symbols import LogSymbols as log_sym  # Enum
+from consolepi import utils
+# from consolepi.exec import ConsolePiExec
+# from consolepi import utils
 # from consolepi.gdrive import GoogleDrive <-- burried import inside refresh method
 
 
 class Remotes():
     '''Remotes Object Contains attributes for discovered remote ConsolePis'''
 
-    def __init__(self, cpi):
+    def __init__(self, config, local, cpiexec):
+        self.cpiexec = cpiexec
         self.pop_list = []
         self.old_api_log_sent = False
         self.log_sym_warn = log_sym.WARNING.value
-        self.cpi = cpi
-        config = cpi.config
+        # self.cpi = cpi
+        self.config = config
+        self.local = local
         self.connected = False
         self.cache_update_pending = False
         self.spin = Halo(spinner='dots')
         self.cloud = None  # Set in refresh method if reachable
-        self.do_cloud = cpi.config.cfg.get('cloud', False)
+        self.do_cloud = config.cfg.get('cloud', False)
         CLOUD_CREDS_FILE = config.static.get('CLOUD_CREDS_FILE')
         if not CLOUD_CREDS_FILE:
             self.no_creds_error()
-        if self.do_cloud and cpi.config.cloud_svc == 'gdrive':
-            if cpi.utils.is_reachable('www.googleapis.com', 443):
+        if self.do_cloud and config.cloud_svc == 'gdrive':
+            if utils.is_reachable('www.googleapis.com', 443):
                 self.local_only = False
-                if not cpi.utils.valid_file(CLOUD_CREDS_FILE):
+                if not utils.valid_file(CLOUD_CREDS_FILE):
                     self.no_creds_error()
             else:
-                cpi.config.log_and_show(f'failed to connect to {cpi.config.cloud_svc} - operating in local only mode',
-                                        log=cpi.config.log.warning)
+                config.plog(f'failed to connect to {config.cloud_svc} - operating in local only mode',
+                            level="warning")
                 self.local_only = True
-        self.data = self.get_remote(data=cpi.config.remotes)
+        self.data = self.get_remote(data=config.remote_update())
 
     def no_creds_error(self):
-        cpi = self.cpi
-        cloud_svc = cpi.config.cfg.get('cloud_svc', 'UNDEFINED!')
-        cpi.config.log_and_show(f'Required {cloud_svc} credentials files are missing refer to GitHub for details',
-                                log=cpi.config.log.warning)
-        cpi.config.log_and_show(f'Disabling {cloud_svc} updates', log=cpi.config.log.warning)
-        cpi.config.error_msgs.append('Cloud Function Disabled by script - No Credentials Found')
-        self.do_cloud = cpi.config.cfg['do_cloud'] = False
+        config = self.config
+        cloud_svc = config.cfg.get('cloud_svc', 'UNDEFINED!')
+        config.plog(f'Required {cloud_svc} credentials files are missing refer to GitHub for details',
+                    log=config.log.warning)
+        config.plog(f'Disabling {cloud_svc} updates', level="warning")
+        config.plog('Cloud Function Disabled by script - No Credentials Found', log=False)
+        self.do_cloud = config.cfg['do_cloud'] = False
 
     # get remote consoles from local cache refresh function will check/update cloud file and update local cache
     def get_remote(self, data=None):
         spin = self.spin
-        cpi = self.cpi
-        config = cpi.config
+        # cpi_exec = self.exec
+        # cpi = self.cpi
+        config = self.config
         log = config.log
         update_cache = False
         if data is None:
@@ -65,12 +72,13 @@ class Remotes():
             data: dict remote ConsolePi dict with hostname as key
             '''
             this = data[remotepi]
-            update, this = self.api_reachable(remotepi, this)
-            if update:
+            res = self.api_reachable(remotepi, this)
+            this = res.data
+            if res.update:
                 self.cache_update_pending = True
 
-            if not this.get('rem_ip'):
-                log.warning(f'[GET REM] Found {"remotepi"} in Local Cloud Cache: UNREACHABLE')
+            if not res.reachable:
+                log.warning(f'[GET REM] Found {remotepi} in Local Cloud Cache: UNREACHABLE')
                 this['fail_cnt'] = 1 if not this.get('fail_cnt') else this['fail_cnt'] + 1
                 self.pop_list.append(remotepi)
                 self.cache_update_pending = True
@@ -80,7 +88,7 @@ class Remotes():
                     this['fail_cnt'] = 0
                     self.cache_update_pending = True
                 if update_cache:
-                    log.info('[GET REM] Updating Cache - Found {0} in Local Cloud Cache, reachable via {1}'.format(
+                    log.info(f'[GET REM] Updating Cache - Found {0} in Local Cloud Cache, reachable via {1}'.format(
                               remotepi, this['rem_ip']))
 
             data[remotepi] = this
@@ -92,19 +100,20 @@ class Remotes():
             print(self.log_sym_warn + ' No Remotes in Local Cache')
             data = {}  # convert None type to empy dict
 
-        if self.cpi.local.hostname in data:
-            del data[self.cpi.local.hostname]
-            config.log_and_show('Local cache included entry for self - do you have other ConsolePis using the same hostname?')
+        if socket.gethostname() in data:
+            del data[socket.gethostname()]
+            config.plog('Local cache included entry for self - do you have other ConsolePis using the same hostname?')
 
         # Verify Remote ConsolePi details and reachability
         if stdin.isatty():
             spin.start('Querying Remotes via API to verify reachability and adapter data')
         for remotepi in data:
-            # Launch Threads to verify all remotes in parallel
+            # -- // Launch Threads to verify all remotes in parallel \\ --
             threading.Thread(target=verify_remote_thread, args=(remotepi, data), name=f'vrfy_{remotepi}').start()
+            # verify_remote_thread(remotepi, data)  # Non-Threading DEBUG
 
         # -- wait for threads to complete --
-        if not cpi.wait_for_threads(name='vrfy_', thread_type='remote'):
+        if not self.cpiexec.wait_for_threads(name='vrfy_', thread_type='remote'):
             if config.remotes:
                 if stdin.isatty():
                     spin.succeed('[GET REM] Querying Remotes via API to verify reachability and adapter data\n\t'
@@ -122,10 +131,10 @@ class Remotes():
                 for remotepi in self.pop_list:
                     if data[remotepi]['fail_cnt'] >= 3:  # NoQA remove from local cache after 3 failures (cloud or mdns will repopulate if discovered)
                         removed = data.pop(remotepi)
-                        config.log_and_show('[GET REM] {} has been removed from Local Cache after {} failed attempts'.format(
-                            remotepi, removed['fail_cnt']), log=config.log.warning)
+                        config.plog('[GET REM] {} has been removed from Local Cache after {} failed attempts'.format(
+                            remotepi, removed['fail_cnt']), level='warning')
                     else:
-                        cpi.error_msgs.append('Cached Remote \'{}\' is unreachable'.format(remotepi))
+                        config.plog('Cached Remote \'{}\' is unreachable'.format(remotepi), log=False)
 
             # update local cache file if rem_ip or adapter data changed
             data = self.update_local_cloud_file(data)
@@ -138,22 +147,24 @@ class Remotes():
     def refresh(self, rem_update=False):
         # pylint: disable=maybe-no-member
         remote_consoles = None
-        cpi = self.cpi
-        config = cpi.config
-        local = cpi.local
-        config.rows, config.cols = cpi.utils.get_tty_size()
+        cpiexec = self.cpiexec
+        # cpi = self.cpi
+        config = self.config
+        plog = config.plog
+        local = self.local
+        # config.rows, config.cols = utils.get_tty_size()
         log = config.log
         cloud_svc = config.cfg.get('cloud_svc', 'error')
 
         # TODO refactor wait_for_threads to have an all key or accept a list
         with Halo(text='Waiting For threads to complete', spinner='dots1'):
-            if cpi.wait_for_threads() and cpi.wait_for_threads(name='_toggle_refresh'):
-                cpi.error_msgs.append('Timeout Waiting for init or toggle threads to complete try again later or'
-                                      ' investigate logs')
+            if cpiexec.wait_for_threads() and cpiexec.wait_for_threads(name='_toggle_refresh'):
+                config.plog('Timeout Waiting for init or toggle threads to complete try again later or'
+                            ' investigate logs')
                 return
 
         # -- // Update Local Adapters \\ --
-        cpi.local.data = cpi.local.build_local_dict(refresh=True)
+        local.data = local.build_local_dict(refresh=True)
         log.debug(f'Final Data set collected for {local.hostname}: {local.data}')
 
         # -- // Get details from Google Drive - once populated will skip \\ --
@@ -162,7 +173,7 @@ class Remotes():
                 # burried import until I find out why this import takes so @#%$@#% long.  Not imported until 1st refresh is called
                 with Halo(text='Loading Google Drive Library', spinner='dots1'):
                     from consolepi.gdrive import GoogleDrive
-                self.cloud = GoogleDrive(cpi, hostname=local.hostname)
+                self.cloud = GoogleDrive(config, hostname=local.hostname)
                 log.info('[MENU REFRESH] Gdrive init')
 
             # Pass Local Data to update_sheet method get remotes found on sheet as return
@@ -176,7 +187,7 @@ class Remotes():
                 self.spin.succeed(_msg + '\n\tFound {} Remotes via Gdrive Sync'.format(len(remote_consoles)))
             elif 'Gdrive-Error:' in remote_consoles:
                 self.spin.fail('{}\n\t{} {}'.format(_msg, self.log_sym_error, remote_consoles))
-                self.error_msgs.append(remote_consoles)  # display error returned from gdrive module
+                plog(remote_consoles)  # display error returned from gdrive module
             else:
                 self.spin.warn(_msg + '\n\tNo Remotes Found via Gdrive Sync')
             if len(remote_consoles) > 0:
@@ -186,11 +197,12 @@ class Remotes():
                 self.update_local_cloud_file(remote_consoles)
                 self.spin.succeed(_msg)  # no real error correction here
             else:
-                config.log_and_show(f'[MENU REFRESH] No Remote ConsolePis found on {cloud_svc}', log=log.warning)
+                plog(f'[MENU REFRESH] No Remote ConsolePis found on {cloud_svc}', level='warning')
         else:
             if self.do_cloud:
-                cpi.error_msgs.append(f'Not Updating from {cloud_svc} due to connection failure')
-                cpi.error_msgs.append('Close and re-launch menu if network access has been restored')
+                plog(f'Not Updating from {cloud_svc} due to connection failure\n'
+                     'Close and re-launch menu if network access has been restored', log=False)
+                # cpi.error_msgs.append('Close and re-launch menu if network access has been restored')
 
         # Update Remote data with data from local_cloud cache
         config.remote = self.get_remote(data=remote_consoles)
@@ -210,15 +222,15 @@ class Remotes():
         returns:
         dict: The resulting remote console dict representing the most recent data for each remote.
         '''
-        utils = self.cpi.utils
-        config = self.cpi.config
+        # utils = self.utils
+        config = self.config
         local_cloud_file = config.static.get('LOCAL_CLOUD_FILE') if local_cloud_file is None else local_cloud_file
 
         log = config.log
         if len(remote_consoles) > 0:
             # if os.path.isfile(local_cloud_file):
             if current_remotes is None:
-                current_remotes = config.remotes
+                current_remotes = self.data = config.remote_update()
 
         # update current_remotes dict with data passed to function
         if len(remote_consoles) > 0:
@@ -227,7 +239,7 @@ class Remotes():
                     if _ not in remote_consoles:
                         if 'fail_cnt' not in current_remotes[_] or current_remotes[_]['fail_cnt'] < 2:
                             remote_consoles[_] = current_remotes[_]
-                        elif 'fail_cnt' not in remote_consoles[_] and 'fail_cnt' in current_remotes[_]:
+                        elif remote_consoles.get(_) and 'fail_cnt' not in remote_consoles[_] and 'fail_cnt' in current_remotes[_]:
                             remote_consoles[_]['fail_cnt'] = current_remotes[_]['fail_cnt']
                     else:
 
@@ -245,7 +257,7 @@ class Remotes():
 
                         # No Change Detected (data passed to function matches cache)
                         if remote_consoles[_] == current_remotes[_]:
-                            log.info('[CACHE UPD] {} No Change in info detected'.format(_))
+                            log.debug('[CACHE UPD] {} No Change in info detected'.format(_))
 
                         # only factor in existing data if source is not mdns
                         elif 'upd_time' in remote_consoles[_] or 'upd_time' in current_remotes[_]:
@@ -290,7 +302,7 @@ class Remotes():
         adapter dict for remote if successful
         Falsey or response status_code if an error occured.
         '''
-        log = self.cpi.config.log
+        log = self.config.log
         url = f'http://{ip}:5000/api/v1.0/adapters'
 
         headers = {
@@ -332,10 +344,16 @@ class Remotes():
             tuple [0]: Bool, indicating if data is different than cache
                   [1]: dict, Updated ConsolePi dictionary for the remote
         '''
+        class ApiReachableResponse():
+            def __init__(self, update, data, reachable):
+                self.update = update
+                self.data = data
+                self.reachable = reachable
+
         update = False
-        cpi = self.cpi
-        config = cpi.config
-        local = cpi.local
+        # cpi = self.cpi
+        config = self.config
+        local = self.local
         log = config.log
 
         _iface_dict = remote_data['interfaces']
@@ -344,15 +362,18 @@ class Remotes():
                        _iface_dict[_iface].get('ip') not in local.ip_list]
 
         # if inbound data includes rem_ip make sure to try that first
-        if remote_data.get('rem_ip'):
-            if rem_ip_list.index(remote_data['rem_ip']) != 0:
-                rem_ip_list.remove(remote_data['rem_ip'])
-                rem_ip_list.insert(0, remote_data['rem_ip'])
+        for _ip in [remote_data.get('rem_ip'), remote_data.get('last_ip')]:
+            if _ip:
+                if _ip not in rem_ip_list or rem_ip_list.index(_ip) != 0:
+                    rem_ip_list.remove(_ip)
+                    rem_ip_list.insert(0, _ip)
 
+        rem_ip = None
         for _ip in rem_ip_list:
             log.debug(f'[API_REACHABLE] verifying {remote_host}')
             _adapters = self.get_adapters_via_api(_ip)
             if _adapters:
+                rem_ip = _ip  # Remote is reachable
                 if not isinstance(_adapters, int):   # indicates an html error code was returned
                     if isinstance(_adapters, list):  # indicates need for conversion from old api format
                         _adapters = {_adapters[_adapters.index(d)]['dev']: {'config': {k: _adapters[_adapters.index(d)][k]
@@ -366,14 +387,32 @@ class Remotes():
                             remote_data['adapters'] = _adapters
                             update = True
                 elif _adapters == 200:
-                    config.log_and_show(f"Remote {remote_host} is reachable via {_ip},"
-                                        " but has no adapters attached\nit's still available in remote shell menu",
-                                        logit=False)
+                    config.plog(f"Remote {remote_host} is reachable via {_ip},"
+                                " but has no adapters attached\nit's still available in remote shell menu",
+                                log=False)
 
-                # remote was reachable update rem_ip, even if returned bad status_code still reachable
-                if not remote_data.get('rem_ip', '') == _ip:
-                    remote_data['rem_ip'] = _ip
-                    update = True  # update if rem_ip didn't match
+                # remote was reachable update last_ip, even if returned bad status_code still reachable
+                if not remote_data.get('last_ip', '') == _ip:
+                    remote_data['last_ip'] = _ip
+                    update = True
                 break
 
-        return (update, remote_data)
+        if remote_data.get('rem_ip') != rem_ip:
+            remote_data['rem_ip'] = rem_ip
+            update = True  # update if rem_ip didn't match
+
+        if not _adapters:
+            reachable = False
+            if isinstance(remote_data.get('adapters'), list):
+                _adapters = remote_data.get('adapters')
+                _adapters = {_adapters[_adapters.index(d)]['dev']: {'config': {k: _adapters[_adapters.index(d)][k]
+                             for k in _adapters[_adapters.index(d)]}} for d in _adapters}
+                remote_data['adapters'] = _adapters
+                _msg = f'{remote_host} Cached adapter data was in old format... Converted to new.\n' \
+                       f'\t\t{remote_host} Should be upgraded to the current version of ConsolePi.'
+                config.plog(_msg, log=True, level='warning')
+                update = True
+        else:
+            reachable = True
+
+        return ApiReachableResponse(update, remote_data, reachable)

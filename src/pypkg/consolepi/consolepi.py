e9cf4d878f61fb35213df2a8438e13a5322feb60
wade ~ Pack3tL0ss <wade99 at github>
2020-03-13 02:20:42 -0500

    cleanup comments

--- src/pypkg/consolepi/consolepi.py
+++ src/pypkg/consolepi/consolepi.py
@@ -1,20 +1,9 @@
 #!/etc/ConsolePi/venv/bin/python3
 
-# import threading
-# import time
-# import os
-# import json
-# import subprocess
-# from consolepi import log
 from consolepi import Response
-# from consolepi import config
-# from consolepi.config import Config
-# from consolepi.utils import Utils
 from consolepi import utils, config
 from consolepi.remotes import Remotes
-# from consolepi.udevrename import Rename
 from consolepi.local import Local
-# from collections import namedtuple
 from consolepi.power import Outlets
 from consolepi.exec import ConsolePiExec
 from consolepi.menu import Menu  # NoQA
@@ -23,11 +12,6 @@ from consolepi.menu import Menu  # NoQA
 class ConsolePi():
     def __init__(self, bypass_remotes=False):
         self.response = Response
-        # self.utils = utils
-        # self.config = config
-        # self.error_msgs = config.error_msgs
-        # self.log = self.config.log
-        # self.plog = self.config.plog
         self.menu = Menu()
         self.local = Local()
         if config.cfg.get('power'):
@@ -39,7 +23,8 @@ class ConsolePi():
         self.cpiexec = ConsolePiExec(config, self.pwr, self.local, self.menu)
         if not bypass_remotes:
             self.remotes = Remotes(self.local, self.cpiexec)
-        # verify TELNET is installed and install if not if hosts of type TELNET are defined.
+
         # TODO Move to menu launch and prompt user
+        # verify TELNET is installed and install if not if hosts of type TELNET are defined.
         if config.hosts:
             utils.verify_telnet_installed(config.hosts)

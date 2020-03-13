fb8de97c277b9fd5b3cd86b99b8e0c01de450393
wade ~ Pack3tL0ss <wade99 at github>
2020-03-13 02:20:01 -0500

    flak8

--- src/consolepi-api.py
+++ src/consolepi-api.py
@@ -4,7 +4,7 @@
 ConsolePi API
 
 Used for remote discovery.  When ConsolePis discover each other
-this allows any of them to display options for all discovered 
+this allows any of them to display options for all discovered
 ConsolePis in consolepi-menu (you can connect to serail devices
 attached to the remotes).
 
@@ -21,10 +21,10 @@ config = ConsolePi_data(do_print=False)
 if config.power:
     if not config.wait_for_threads():
         outlets = config.pwr.outlet_data if config.pwr.outlet_data else None
-else: 
+else:
     outlets = None
 log = config.log
-user = config.USER # pylint: disable=maybe-no-member
+user = config.USER  # pylint: disable=maybe-no-member
 last_update = int(time())
 
 app = FastAPI(title='ConsolePi.API',
@@ -33,8 +33,10 @@ app = FastAPI(title='ConsolePi.API',
               openapi_url='/api/openapi/openapi.json'
               )
 
+
 def log_request(request: Request, route: str):
     log.info('[NEW API RQST IN] {} Requesting -- {} -- Data via API'.format(request.client.host, route))
+
 
 @app.get('/api/v1.0/adapters')
 def adapters(request: Request):
@@ -44,23 +46,26 @@ def adapters(request: Request):
     # subsequent API calls from all other ConsolePi on the network
     global last_update
     if int(time()) - last_update > 20:
-        adapters  = config.get_adapters(do_print=False)
+        adapters = config.get_adapters(do_print=False)
         last_update = int(time())
         return {'adapters': adapters}
     else:
         return {'adapters': config.adapters}
 
+
 @app.get('/api/v1.0/remotes')
 def remotes(request: Request):
     log_request(request, 'remotes')
-    remotes  = config.get_local_cloud_file()
+    remotes = config.get_local_cloud_file()
     return {'remotes': remotes}
+
 
 @app.get('/api/v1.0/interfaces')
 def get_ifaces(request: Request):
     log_request(request, 'ifaces')
     ifaces = config.get_if_ips()
     return {'interfaces': ifaces}
+
 
 @app.get('/api/v1.0/outlets')
 def get_outlets(request: Request, outlets=outlets):
@@ -75,6 +80,7 @@ def get_outlets(request: Request, outlets=outlets):
                         del outlets['linked'][grp][x]
     return outlets
 
+
 @app.get('/api/v1.0/details')
 def get_details(request: Request):
     log_request(request, 'details')
@@ -83,6 +89,7 @@ def get_details(request: Request):
 # @app.get("/api/v1.0/{item_id}")
 # def read_item(self, item_id: int, q: str = None):
 #     return {"item_id": item_id, "q": q}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5000, log_level="info")

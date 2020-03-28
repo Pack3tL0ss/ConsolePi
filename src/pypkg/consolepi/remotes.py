3fe68657bdca18e72d46ce94de65b1a989f3adea
wade ~ Pack3tL0ss <wade99 at github>
2020-03-28 02:35:37 -0500

    handle No Adapters for remote in update_local_cloud_file

--- src/pypkg/consolepi/remotes.py
+++ src/pypkg/consolepi/remotes.py
@@ -259,8 +259,8 @@ class Remotes():
                                     log.info('[CACHE UPD] {} Updating data from {} '
                                              'based on more current update time'.format(_, remote_consoles[_]['source']))
                                 else:  # -- Update Times are equal --
-                                    if remote_consoles[_].get('adapters', {'adapters': {}})['adapters'].keys() != \
-                                       current_remotes[_].get('adapters', {'adapters': {}})['adapters'].keys() or \
+                                    if (current_remotes[_].get('adapters') and remote_consoles[_].get('adapters') and 
+                                        current_remotes[_]['adapters'].keys() != remote_consoles[_]['adapters'].keys()) or \
                                        remote_consoles[_].get('interfaces', {}) != \
                                        current_remotes[_].get('interfaces', {}):
                                         log.warning('[CACHE UPD] {} current cache update time and {} update time are equal'

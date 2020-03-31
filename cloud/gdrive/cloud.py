6e92398e680e664d39b0b9d9abbec17555f6c857
wade ~ Pack3tL0ss <wade99 at github>
2020-03-30 19:00:34 -0500

    re-write for new library

--- cloud/gdrive/cloud.py
+++ cloud/gdrive/cloud.py
@@ -2,14 +2,33 @@
 
 import sys
 sys.path.insert(0, '/etc/ConsolePi/src/pypkg')
-from consolepi import log  # NoQA
+from consolepi import config, log  # NoQA
 from consolepi.consolepi import ConsolePi  # NoQA
+from consolepi.gdrive import GoogleDrive # NoQA
 
 
 def main():
     cpi = ConsolePi()
+    cloud_svc = config.cfg.get("cloud_svc", "error")
+    local = cpi.local
+    remotes = cpi.remotes
+    cpiexec = cpi.cpiexec
     log.info('[CLOUD TRIGGER (IP)]: Cloud Update triggered by IP Update')
-    cpi.remotes.refresh()
+
+    # -- // Get details from Google Drive - once populated will skip \\ --
+    if not remotes.local_only:
+        if cloud_svc == "gdrive" and remotes.cloud is None:
+            remotes.cloud = GoogleDrive(hostname=local.hostname)
+
+        if cpiexec.wait_for_threads(thread_type="remotes") and (
+           config.power and cpiexec.wait_for_threads(name="_toggle_refresh")):
+            log.error('IP Change Cloud Update Trigger: TimeOut Waiting for Threads to Complete')
+
+        remote_consoles = remotes.cloud.update_files(local.data)
+        if "Gdrive-Error:" in remote_consoles:
+            log.error(remote_consoles)
+    else:
+        log.error(f"Not Updating {cloud_svc} due to connection failure")
 
 
 if __name__ == '__main__':

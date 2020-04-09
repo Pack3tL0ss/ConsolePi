041930bdcba0cd51cfbb4162705b970110239c53
wade ~ Pack3tL0ss <wade99 at github>
2020-04-09 02:52:14 -0500

    re-factor for new library, re-write, make more awesome

--- src/consolepi-details.py
+++ src/consolepi-details.py
@@ -1,54 +1,75 @@
 #!/etc/ConsolePi/venv/bin/python3
 
-import json
 import sys
-import os
-from halo import Halo
-from consolepi.common import (ConsolePi_data, set_perm)
+sys.path.insert(0, '/etc/ConsolePi/src/pypkg')
+from consolepi import config, utils, log  # NoQA
+from consolepi.consolepi import ConsolePi  # NoQA
 
-config = ConsolePi_data(do_print=False)
-local_cloud_file = config.LOCAL_CLOUD_FILE # pylint: disable=maybe-no-member
-spin = Halo(spinner='dots')
 
-spin.start('Gathering ConsolePi Data')
-hostname = config.hostname
-adapters = config.adapters
-interfaces = config.interfaces
-outlets = config.pwr.outlet_data
-remotes = config.remotes
-details = config.local_data_repr()
-details['remotes'] = remotes
-spin.stop()
+# local_cloud_file = config.static.get(LOCAL_CLOUD_FILE)
+cpi = ConsolePi()
+local = cpi.local
+hostname = local.hostname
+adapters = local.adapters
+interfaces = local.interfaces
+remotes = cpi.remotes.data
+details = local.data
+dump = {'local': details, 'remotes': config.remotes}
+# details['outlets'] = outlets
+if config.remotes:
+    for r in config.remotes:
+        if r not in cpi.remotes.data:
+            dump['remotes'][r]['!! WARNING !!'] = 'This Device is Currently Unreachable'
+jprint = utils.json_print
+
+
+def get_outlets():
+    if not cpi.cpiexec.pwr_init_complete:
+        utils.spinner('Waiting for Power Threads To Complete', cpi.cpiexec.wait_for_threads)
+        return cpi.pwr.data
+
 
 if len(sys.argv) > 1:
     if sys.argv[1] == 'adapters':
-        print(json.dumps(adapters, indent=4, sort_keys=True))
+        jprint(adapters)
     elif sys.argv[1] == 'interfaces':
-        print(json.dumps(interfaces, indent=4, sort_keys=True))
+        jprint(interfaces)
     elif sys.argv[1] == 'outlets':
-        if not config.pwr_init_complete:
-            config.wait_for_threads()
-            outlets = config.pwr.outlet_data
-        print(json.dumps(outlets, indent=4, sort_keys=True))
+        jprint(get_outlets())
     elif sys.argv[1] == 'remotes':
         if len(sys.argv) == 2:
-            print(json.dumps(remotes, indent=4, sort_keys=True))
+            jprint(remotes)
         elif len(sys.argv) == 4:
             if sys.argv[2] == 'del':
                 if sys.argv[3] in remotes:
                     print('Removing ' + sys.argv[3] + ' from local cloud cache')
                     remotes.pop(sys.argv[3])
-                    config.update_local_cloud_file(remote_consoles=remotes, current_remotes=remotes)
+                    cpi.remotes.update_local_cloud_file(remote_consoles=remotes, current_remotes=remotes)
                     print('Remotes remaining in local cache')
-                    print(json.dumps(remotes, indent=4, sort_keys=True))
+                    jprint(remotes)
                     print('{} Removed from local cache'.format(sys.argv[3]))
+                else:
+                    print(f'!! {sys.argv[3]} Not Found in Local Cache')
     elif sys.argv[1] == 'local':
-        print(json.dumps(details, indent=4, sort_keys=True))
+        jprint(details)
     else:
         try:
-            print('\nRemote Cache data for {}:\n{}'.format(sys.argv[1], json.dumps(details['remotes'][sys.argv[1]], indent=4, sort_keys=True)))
+            print('\nRemote Cache data for {}:\n{}'.format(sys.argv[1], jprint(details['remotes'][sys.argv[1]])))
         except KeyError:
-            print(json.dumps(details, indent=4, sort_keys=True))
+            dump['outlets'] = get_outlets()
+            jprint(dump)
             print('!!!  {} Not Found entire data set above   !!!'.format(sys.argv[1]))
+
 else:
-    print(json.dumps(details, indent=4, sort_keys=True))
+    dump['outlets'] = get_outlets()
+    jprint(dump)
+
+log.show('This is a test failure')
+log.show('So is this')
+
+if log.error_msgs:
+    print('!! ', end='')
+    print('\n!! '.join(log.error_msgs))
+
+if not cpi.cpiexec.pwr_init_complete:
+    utils.spinner('Exiting... Waiting for Power Threads To Complete', cpi.cpiexec.wait_for_threads)

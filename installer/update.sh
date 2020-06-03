5a70b6ff920accc0d64a617e4777560a6d78a6a5
wade ~ Pack3tL0ss <wade99 at github>
2020-06-03 02:13:18 -0500

    Fix missing logit cmd - tz silent.  Set process for silent post msg

--- installer/update.sh
+++ installer/update.sh
@@ -110,7 +110,7 @@ set_timezone() {
             rm /etc/localtime
             echo "$tz" > /etc/timezone
             dpkg-reconfigure -f noninteractive tzdata 2>>$log_file &&
-                "Set new TimeZone to $(date +"%Z") based on tz arg/config option Success" ||
+                logit "Set new TimeZone to $(date +"%Z") based on tz arg/config option Success" ||
                     logit "FAILED to set new TimeZone based on tz arg/config option" "WARNING"
         fi
         unset tz
@@ -1048,6 +1048,7 @@ update_main() {
     if ! $silent; then
         post_install_msg
     else
+        process=Complete
         _msg="Success Silent Install Complete"
         [[ "$warn_cnt" > 0 ]] && _msg="$msg Warnings Exist ($warn_cnt)"
         logit "${_msg}"; printf '\a'

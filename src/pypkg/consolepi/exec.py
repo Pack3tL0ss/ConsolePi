ea6f07f6b7811f9dcd0af5a5d08759da528d7f4f
wade ~ Pack3tL0ss <wade99 at github>
2020-09-16 22:11:47 -0500

    handle keyboard Interupt in remote shell menu. handle EoFError in all places where KeyboardInterupt...

--- src/pypkg/consolepi/exec.py
+++ src/pypkg/consolepi/exec.py
@@ -347,7 +347,7 @@ class ConsolePiExec:
 
         else:
             ch = choice.lower
-            try:  # Invalid Selection
+            try:  # Invalid Selection, Keyboard Interupt
                 if isinstance(menu_actions[ch], dict):
                     if menu_actions[ch].get("cmd"):
                         # TimeStamp for picocom session log file if defined
@@ -395,7 +395,7 @@ class ConsolePiExec:
                                                     break
                                                 else:
                                                     time.sleep(3)
-                                            except KeyboardInterrupt:
+                                            except (KeyboardInterrupt, EOFError):
                                                 self.autopwr_wait = False
                                                 log.show("Connection Aborted")
                                                 break
@@ -428,7 +428,7 @@ class ConsolePiExec:
                                     "/etc/ConsolePi/src/consolepi-commands/resize >/dev/null"
                                 )
 
-                        except KeyboardInterrupt:
+                        except (KeyboardInterrupt, EOFError):
                             log.show("Aborted last command based on user input")
 
                     elif "function" in menu_actions[ch]:
@@ -669,6 +669,9 @@ class ConsolePiExec:
             except KeyError as e:
                 if len(choice.orig) <= 2 or not self.exec_shell_cmd(choice.orig):
                     log.show(f"Invalid selection {e}, please try again.")
+            except (KeyboardInterrupt, EOFError):
+                log.show('Operation Aborted')
+                print('')  # prevents header and prompt on same line in debug
         return True
 
     def confirm_and_spin(self, action_dict, *args, **kwargs):
@@ -759,7 +762,7 @@ class ConsolePiExec:
                         else str(port) + "(" + port_name + ")",
                     )
                 )
-            except KeyboardInterrupt:
+            except (KeyboardInterrupt, EOFError):
                 name = None
                 confirmed = False
                 print("")  # So header doesn't print on same line as aborted prompt when DEBUG is on

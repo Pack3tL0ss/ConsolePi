61ad15e6298da2408c5ac69293dfa537b52af011
wade ~ Pack3tL0ss <wade99 at github>
2020-12-02 18:33:50 -0600

    fix argument logic

--- src/consolepi-wlanreset.sh
+++ src/consolepi-wlanreset.sh
@@ -29,28 +29,48 @@ start_stop_dnsmasq() {
     fi
 }
 
-wpa_cli terminate >/dev/null
-ip addr flush wlan0 2>/dev/null
-ip link set dev wlan0 down
+wpa_cli terminate >/dev/null 2>&1
+ip addr flush $wifidev 2>/dev/null
+ip link set dev $wifidev down
 systemctl stop hostapd >/dev/null 2>&1
 start_stop_dnsmasq stop
 
-[ -f /var/run/wpa_supplicant/$wifidev ] && rm /var/run/wpa_supplicant/$wifidev
+# [ -f /var/run/wpa_supplicant/$wifidev ] && rm /var/run/wpa_supplicant/$wifidev
+_ctrl_if=$(grep "^ctrl_interface" /etc/wpa_supplicant/wpa_supplicant.conf)
+_ctrl_if=$(echo ${x// GROUP*/} | rev | cut -d= -f 1 | rev)
+[ -f "$_ctrl_if/$wifidev" ] && rm "$_ctrl_if/$wifidev" >/dev/null 2>&1
 rmdir /var/run/wpa_supplicant 2>/dev/null
-rm -r /tmp/wlan0 2>/dev/null
+rm -r /tmp/$wifidev 2>/dev/null
 
 iptables -D FORWARD -i "$ethdev" -o "$wifidev" -m state --state RELATED,ESTABLISHED -j ACCEPT 2>/dev/null
 iptables -D FORWARD -i "$wifidev" -o "$ethdev" -j ACCEPT 2>/dev/null
 echo 0 > /proc/sys/net/ipv4/ip_forward
 
-ip link set dev wlan0 up
+ip link set dev $wifidev up 2>&1
+
+# wait for interface to come up
+idx=0; while ip link show dev $wifidev >/dev/null | grep -q "state DOWN"; do
+    [[ $idx == 0 ]] && printf "\nWaiting for $wifidev to come back up..."
+    sleep 3 ; printf "..."
+    ((idx+=1)); printf $idx
+    if [[ $idx > 5 ]]; then
+        break
+    fi
+done
+echo
+
 dhcpcd  -n "$wifidev" >/dev/null 2>&1
-if [ -z "$1" ]; then  # Any arg will prevent wpa_from starting
-    wpa_supplicant -B -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf
-elif [[ "$1" =~ debug ]]; then
+
+if [[ $1 =~ debug ]]; then
+    echo -e "\nStarting wpa_supplicant in forground with debug"
+    echo 'wpa_supplicant -dd -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf'
+    echo
     wpa_supplicant -dd -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf
-else
-    echo "wpa_supplicant not started based on "$1" arg"
+elif [ -z "$1" ]; then
+    wpa_res=$(wpa_supplicant -B -i "$wifidev" -c /etc/wpa_supplicant/wpa_supplicant.conf 2>&1) &&
+        echo -e "${wpa_res}"| head -1 || echo -e "${wpa_res[@]}"
+else  # Any arg will prevent wpa_from starting
+    echo -e "\nwpa_supplicant not started based on "$1" arg"
     echo "  -debug   Starts wpa_supplicant in terminal w/ debug"
-    echo "  Any Other Arg kills hotspot but does not restart wpa_supplicant"
+    echo -e "  Any Other Arg kills hotspot but does not restart wpa_supplicant\n"
 fi
